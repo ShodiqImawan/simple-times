ab7c4a9940c251ace413e453805e7a95d757d242
shodiqimawan <shodiqsantuy@gmail.com>
2025-04-10 08:44:37 +0700

    Simple Times

---
+++ times.js
@@ -0,0 +1,44 @@
+let second = 0
+let minute = 0
+let intervalId = null
+
+function timeStart () {
+  
+  if (intervalId !== null) {
+    console.log(`Waktu sudah berjalan!`)
+    return
+  }
+  
+  intervalId = setInterval(() => {
+    second += 1;
+    
+    if (second == 60) {
+      minute += 1;
+      second = 0;
+    }
+    
+    console.log(`${minute}:${second}`);
+  }, 1000);
+}
+
+function timeStop () {
+  clearInterval(intervalId);
+  intervalId = null;
+  console.log(`Waktu di hentikan pada ${minute}:${second}`)
+  return
+}
+
+function resetTime () {
+  console.log(`Waktu terakhir sebelum di reset ${minute}:${second}`);
+  
+  setTimeout(() => {
+    timeStop();
+    console.log(`Waktu sedang di reset`);
+    setTimeout(() => {
+      second = 0;
+      minute = 0;
+      console.log(`Waktu berhasil di reset!`)
+    }, 3000);
+  }, 2000);
+  return
+}
